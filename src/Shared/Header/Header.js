9f3ecc0d9f883afa634e1856482cb53d92039f90
Raj106292 <rajdas.106292@gmail.com>
2023-01-06 01:07:14 +0600

    some style on navbar

--- src/Shared/Header/Header.js
+++ src/Shared/Header/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
 
     const handleUserLogOut = () => {
         const process = window.confirm('Are you sure to log out?');
-        if(process){
+        if (process) {
             userLogOut();
             toast.success('Log Out Successful');
         }
@@ -37,18 +37,31 @@ const Header = () => {
                 <Navbar.Collapse>
                     <Link
                         to="/"
-                        className='font-semibold text-lg'
+                        className='font-semibold text-lg flex items-center flex-shrink-0 py-1 px-2 mb-2 lg:mb-0 space-x-1 border-b-2 hover:border-t hover:border-r-4 hover:border-l hover:border-b-0 hover:rounded-t-lg hover:bg-slate-200 hover:text-blue-700'
                     >
-                        Home
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                        </svg>
+                        <span>Home</span>
                     </Link>
-                    <Link to="/add-task" className='font-semibold text-lg'>
-                        Add Task
+                    <Link to="/add-task" className='font-semibold text-lg flex items-center flex-shrink-0 py-1 px-2 mb-2 lg:mb-0 space-x-1 border-b-2 hover:border-t hover:border-r-4 hover:border-l hover:border-b-0 hover:rounded-t-lg hover:bg-black hover:text-white'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
+                        </svg>
+                        <span>Add Task</span>
                     </Link>
-                    <Link to="/navbars" className='font-semibold text-lg'>
-                        My Task
+                    <Link to="/navbars" className='font-semibold text-lg flex items-center flex-shrink-0 py-1 px-2 mb-2 lg:mb-0 space-x-1 border-b-2 hover:border-t hover:border-r-4 hover:border-l hover:border-b-0 hover:rounded-t-lg hover:bg-slate-200 hover:text-blue-700'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z" />
+                        </svg>
+
+                        <span>My Task</span>
                     </Link>
-                    <Link to="/navbars" className='font-semibold text-lg'>
-                        Completed Task
+                    <Link to="/navbars" className='font-semibold text-lg flex items-center flex-shrink-0 py-1 px-2 mb-2 lg:mb-0 space-x-1 border-b-2 hover:border-t hover:border-r-4 hover:border-l hover:border-b-0 hover:rounded-t-lg hover:bg-black hover:text-white'>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M11.35 3.836c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m8.9-4.414c.376.023.75.05 1.124.08 1.131.094 1.976 1.057 1.976 2.192V16.5A2.25 2.25 0 0118 18.75h-2.25m-7.5-10.5H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V18.75m-7.5-10.5h6.375c.621 0 1.125.504 1.125 1.125v9.375m-8.25-3l1.5 1.5 3-3.75" />
+                        </svg>
+                        <span>Completed Task</span>
                     </Link>
                     {
                         user?.email ?
@@ -56,7 +69,10 @@ const Header = () => {
                                 <button onClick={handleUserLogOut} type="button" className="font-semibold text-lg rounded hover:text-blue-600 dark:bg-gray-100 dark:text-gray-800">Log Out</button>
                             </div>
                             :
-                            <Link to="/login" className='font-semibold text-lg'>
+                            <Link to="/login" className='font-semibold text-lg flex items-center flex-shrink-0 py-1 px-2 mb-2 lg:mb-0 space-x-1 border-b-2 hover:border-t hover:border-r-4 hover:border-l hover:border-b-0 hover:rounded-t-lg hover:bg-slate-200 hover:text-blue-700'>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
+                                </svg>
                                 Log In
                             </Link>
                     }
